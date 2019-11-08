78ea4377d0e56623a22035d67e0497b25e903462
Kharoh <Gaulu03@gmail.com>
2019-11-08 09:52:57 +0100

    now rendering Header and Lesson

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+import Header from './Header/Header'
+import Lesson from './Lesson/Lesson'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div id="App">
+        <Header />
+        <Lesson />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
